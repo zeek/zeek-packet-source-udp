35e5ac956f6a99217b27a8ce312e629ba5d370d7
Arne Welzel <arne.welzel@corelight.com>
2026-04-23 10:46:55 +0200

    packet_source_debug: Allow macro for UDPSOURCE_DEBUG()

--- src/packet_source_debug.h
+++ src/packet_source_debug.h
@@ -2,6 +2,7 @@
 
 #include "plugin.h" // For the plugin instance.
 
+// NOLINTNEXTLINE(cppcoreguidelines-macro-usage)
 #define UDPSOURCE_DEBUG(...) PLUGIN_DBG_LOG(::plugin::Zeek_PacketSourceUDP::plugin, __VA_ARGS__)
 
 // Avoid plugin.h unused for non-debug builds.
